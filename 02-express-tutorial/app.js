9741e45049772b481b504761d6e8b7ac882f229c
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-03 10:11:24 -0400

    create basic express server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,11 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  console.log(req)
+  res.writeHead(200,{'content-type':'text/html'})
+  res.write('<h1>home page</h1>')
+  res.end()
+})
+
+
+server.listen(5000)
