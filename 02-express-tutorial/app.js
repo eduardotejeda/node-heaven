957678354cdbb0190ad1b20465fe4525167640fc
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-04 08:22:34 -0400

    Add index.html to open with readFilesSync

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,11 +1,31 @@
 const http = require('http')
+const {readFileSync} = require('fs');
 
-const server = http.createServer((req, res) => {
-  console.log(req)
-  res.writeHead(200,{'content-type':'text/html'})
-  res.write('<h1>home page</h1>')
-  res.end()
-})
+// get all files
+const homePage = readFileSync('./index.html')
 
+const server = http. createServer((req, res) => {
+  // console. log(req. method)
+   const url = req.url
+  // home page
+   if (url === '/') {
+    res.writeHead(200, {'content-type': 'text/html' })
+    res.write(homePage)
+    res.end()
+   }
+  // about page
+   else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write( '<h1>about page</h1>')
+    res.end()
+  }
+  // 404
+   else {
+    res.writeHead(404, {'content-type': 'text/html' })
+    res.write('sh1>page not found</h1>')
+    res.end()
+   }
+
+  })
 
 server.listen(5000)
