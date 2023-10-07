d2a9b40979628013e2814707d053785f2add2e55
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-07 08:35:52 -0400

    Remove global not found route

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,34 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const {products} = require('./data')
 
-app.use(express.static('./public'))
-
-// app.get('/', (req, res)=> {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))  
-//   adding to static assets
-//   SSR
-// }) 
-
-app.all('*', (req, res) => {
-  res.status(404).send('Not found 404')
+app.get('/', (req, res)=> {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>')  
 })
 
+app.get('/api/products', (req, res)=> {
+  const newProducts = products.map((product) => {
+    const {id, name, image} = product
+    return{ id, name, image }
+  })
+res.json(newProducts)
+})
 
-app.listen(5000, ()=> {
-  console.log('Listening on port 5000')
+app.get('/api/products/:productID', (req, res)=> {
+  //console.log(req)
+  //console.log(req.params)
+  const { productID } = req.params
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+    )
+    if(!singleProduct) {
+      return res.status(404).send('<h1>Resource not found</h1>')
+    } 
+    return  res.json(singleProduct)
+
+})
+
+app.listen(5000, () => {
+  console.log( "Listening on port 5000......")
 })
