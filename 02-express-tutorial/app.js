cc1a681cd9de9450d13a3a812018bf7e4eab1b7e
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-08 19:40:59 -0400

    Add query example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -30,8 +30,28 @@ app.get('/api/products/:productID', (req, res)=> {
 })
 
 app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
+  
+  
+})
+
+app.get('/api/v1/query', (req, res) => {
+  const {search, limit} = req.query
+  let sortedProducts = [...products]
+
+  if(search) {
+    sortedProducts = sortedProducts.filter((product)=> {
+      return product.name.startsWith(search)
+    })
+  }
+  if(limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+  if(sortedProducts.length < 1) {
+    // res.status(200).send('No products with that letter')
+    return res.status(200).json({sucess: true, data: []})
+
+  }
+  res.status(200).json(sortedProducts)
 })
 
 
