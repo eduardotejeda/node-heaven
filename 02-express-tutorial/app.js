8f42b7a9b72d0fbcfb6d9fcce41cfe8a270c65df
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-09 00:38:22 -0400

    Add midleware examples

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,60 +1,21 @@
-const express = require('express');
-const app = express();
-const {products} = require('./data')
+const { ur } = require("@faker-js/faker")
+const express = require('express')
+const app = express()
+const logger = require('./logger')
 
-app.get('/', (req, res)=> {
-  res.send('<h1>Home Page</h1><a href="/api/products">products</a>')  
-})
 
-app.get('/api/products', (req, res)=> {
-  const newProducts = products.map((product) => {
-    const {id, name, image} = product
-    return{ id, name, image }
+// req => middleware => res
+
+
+
+app.get('/', logger, (req, res) => { 
+  res.send('Home')
   })
-res.json(newProducts)
-})
-
-app.get('/api/products/:productID', (req, res)=> {
-  //console.log(req)
-  //console.log(req.params)
-  const { productID } = req.params
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-    )
-    if(!singleProduct) {
-      return res.status(404).send('<h1>Resource not found</h1>')
-    } 
-    return  res.json(singleProduct)
-  
-})
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  
-  
-})
-
-app.get('/api/v1/query', (req, res) => {
-  const {search, limit} = req.query
-  let sortedProducts = [...products]
-
-  if(search) {
-    sortedProducts = sortedProducts.filter((product)=> {
-      return product.name.startsWith(search)
-    })
-  }
-  if(limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-  if(sortedProducts.length < 1) {
-    // res.status(200).send('No products with that letter')
-    return res.status(200).json({sucess: true, data: []})
-
-  }
-  res.status(200).json(sortedProducts)
+app.get('/about', logger, (req, res) => {
+  res.send('About')
 })
 
 
 app.listen(5000, () => {
-  console.log( "Listening on port 5000......")
+  console.log('Server is listening on port 5000...')
 })
