1c18f9d7a4d0e811c71cfdc38cf6a4546dfcc12d
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-09 22:23:26 -0400

    Forms with express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,17 @@
-const { ur } = require("@faker-js/faker")
 const express = require('express')
 const app = express()
-const logger = require('./logger')
+const {people} = require('./data')
 
-
-// req => middleware => res
-
-
-
-app.get('/', logger, (req, res) => { 
-  res.send('Home')
-  })
-app.get('/about', logger, (req, res) => {
-  res.send('About')
+//static assets
+app.use(express.static('./methods-public'))
+// parse from data
+app.get('/api/people', (req, res)=> {
+  res.status(200).json({success:true, data:people})
 })
 
+app.post('/login', (req, res) => {
+  res.send('POST')
+})
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000...')
