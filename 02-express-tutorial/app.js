18a2c639af2933492a5aa1c1e9b2e274f81a7417
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-10 10:04:20 -0400

    Add new function

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const app = express()
 const {people} = require('./data')
+const { fa } = require("@faker-js/faker")
 
 //static assets
 app.use(express.static('./methods-public'))
 // parse from data
+app.use(express.urlencoded({extended:false}))
 app.get('/api/people', (req, res)=> {
   res.status(200).json({success:true, data:people})
 })
 
 app.post('/login', (req, res) => {
+  const {name} = req.body;
+  if(name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please Provide Credentials')
+
+
   res.send('POST')
 })
 
