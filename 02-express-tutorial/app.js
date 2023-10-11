39da3ee4dd1a00ac8383cab9637d7e97e03f67ae
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-11 06:44:31 -0400

    Add way to test using Postman

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -17,9 +17,11 @@ app.get('/api/people', (req, res)=> {
 app.post('/api/people', (req, res)=> {
   const {name} = req.body
   if(!name){
-    return res.status(400).json({success:false, msg:'please provide name value'})
+    return res
+    .status(400)
+    .json({success:false, msg:'please provide name value'})
   }
-  res.status(201).json({})
+  res.status(201).json({success: true, person:name})
 })
 
 app.post('/login', (req, res) => {
