49324dad87313d9c3fcc06268715a8427431e5c4
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-10 23:00:21 -0400

    add post example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,8 +7,19 @@ const { fa } = require("@faker-js/faker")
 app.use(express.static('./methods-public'))
 // parse from data
 app.use(express.urlencoded({extended:false}))
+// parse json
+app.use(express.json())
+
 app.get('/api/people', (req, res)=> {
   res.status(200).json({success:true, data:people})
+})
+
+app.post('/api/people', (req, res)=> {
+  const {name} = req.body
+  if(!name){
+    return res.status(400).json({success:false, msg:'please provide name value'})
+  }
+  res.status(201).json({})
 })
 
 app.post('/login', (req, res) => {
