87d323ce89a5a70a25f586535dc0ee88718caae1
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-15 16:32:50 -0400

    Add improve routes and auth

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+
 const people = require('./routes/people')
+const auth = require('./routes/auth')
+
 
 //static assets
 app.use(express.static('./methods-public'))
@@ -10,15 +13,8 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 app.use('/api/people', people)
+app.use('/login', auth)
 
-app.post('/login', (req, res) => {
-  const {name} = req.body;
-  if(name) {
-    return res.status(200).send(`Welcome ${name}`)
-  }
-
-  res.status(401).send('Please Provide Credentials')
-})
 
 
 app.listen(5000, () => {
