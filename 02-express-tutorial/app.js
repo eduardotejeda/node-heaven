e09a3e464bada549c83e6011412913ce3b0f2d3a
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-10-15 12:54:53 -0400

    Routes added

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-const {people} = require('./data')
-const { fa } = require("@faker-js/faker")
+const people = require('./routes/people')
 
 //static assets
 app.use(express.static('./methods-public'))
@@ -10,30 +9,7 @@ app.use(express.urlencoded({extended:false}))
 // parse json
 app.use(express.json())
 
-app.get('/api/people', (req, res)=> {
-  res.status(200).json({success:true, data:people})
-})
-
-app.post('/api/people', (req, res)=> {
-  const {name} = req.body
-  if(!name){
-    return res
-    .status(400)
-    .json({success:false, msg:'please provide name value'})
-  }
-  res.status(201).json({success: true, person:name})
-})
-
-app.post('/api/postman/people', (req, res)=> {
-  console.log(req)
-  const {name} = req.body
-  if(!name) {
-    return res
-    .status(400)
-    .json({success:false, msg:'please provide name value'})
-  }
-  res.status(201).json({success: true, data:[...people, name]})
-})
+app.use('/api/people', people)
 
 app.post('/login', (req, res) => {
   const {name} = req.body;
@@ -44,25 +20,6 @@ app.post('/login', (req, res) => {
   res.status(401).send('Please Provide Credentials')
 })
 
-app.put('/api/people/:id', (req, res) => {
-const {id} = req.params
-const {name} = req.body
-
-const person = people.find((person)=> person.id === Number(id))
-
-if(!person){
-  return res
-  .status(400)
-  .json({success: false, msg: `no person with id ${id}`})
-}
-const newPeople = people.map((person) => {
-  if(person.id === Number(id)){
-    person.name = name
-  }
-  return person
-})
-res.status(200).json({success:})
-})
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000...')
